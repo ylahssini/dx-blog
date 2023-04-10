87a003c7e304684f7b3ba0409cbcaf0d9e9db0bb
ylahssini <ylahssini@gmail.com>
2023-04-10 01:25:32 +0000

    ⚰️ remove search bar in header

--- dxblog-admin/components/layout/main/header/index.tsx
+++ dxblog-admin/components/layout/main/header/index.tsx
@@ -1,6 +1,5 @@
 import dynamic from 'next/dynamic';
-import { Box, Flex, Input, InputGroup, InputRightElement, Button, Text } from '@chakra-ui/react';
-import { MdSearch } from 'react-icons/md';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import { useIsConnected } from '@/utils/hooks';
 
 const Avatar = dynamic(() => import('@/components/avatar'), { ssr: true });
@@ -11,19 +10,6 @@ export default function Header({ title }) {
     return (
         <Flex as="header" p="2rem" color="var(--primary)" fontSize="2rem" fontWeight={700} alignItems="center" justifyContent="space-between">
             <h1>{title}</h1>
-            <InputGroup maxW={400}>
-                <Input
-                    color="var(--primary)"
-                    placeholder="Find order, customer or product"
-                    _placeholder={{ color: 'inherit', fontWeight: 400 }}
-                />
-
-                <InputRightElement>
-                    <Button h="1.75rem" size="sm" type="button" variant="ghost">
-                        <MdSearch />
-                    </Button>
-                </InputRightElement>
-            </InputGroup>
             <Box display="flex" alignItems="center" justifyContent="flex-end" gap="1rem" color="black">
                 <Box as="header" textAlign="right" fontSize="sm" lineHeight={1}>
                     <Text as="small" fontWeight={300}>Hello</Text><br />
